9e9d56e8783fa703924ebf38fbe60d4a40427fef
nvjome <nvjome@gmail.com>
2022-01-09 17:06:19 -0600

    LUT coding, begin function edits, effect 1

--- T9_Pedal_Bundle.cpp
+++ T9_Pedal_Bundle.cpp
@@ -6,7 +6,12 @@
 #include "Arduino.h"
 #include "T9_Pedal_Bundle.h"
 
+/*
+    T9PB_begin
+    Initializes the Teensy Audio library and settings
+*/
 void T9PB_begin(int memory) {
+    T9PB_disconnect_all_cords();
 
     sgtl5000.enable();
     sgtl5000.inputSelect(AUDIO_INPUT_LINEIN);
--- T9_Pedal_Bundle.h
+++ T9_Pedal_Bundle.h
@@ -13,31 +13,28 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+// Number of implemented effects
+#define NUM_EFFECTS 1
+
 ///////////////////////////////////////
 // Audio Objects
 // All audio objects for each effect, input, output, and others.
+// Use format for effects:  effectXXShortBlockName
+// Where XX is effect number.
 ///////////////////////////////////////
 
 // System input objects:
 AudioInputI2S               lineIn;
 AudioAmplifier              preGain;
 
-// Effect 1: 
-
+// Effect 1: Low Pass Filter
+AudioFilterStateVariable    effect1LPF;
 
 // Effect 2: 
 
 
 // Effect 3: 
 
-
-// Effect 4: 
-
-
-// Effect 5: 
-
-
-// Effect 6: 
 
 
 // System output objects:
@@ -55,29 +52,48 @@ AudioControlSGTL5000        sgtl5000;
 //                          effectXXOutput
 //                          effectXXSubYY
 // Where XX is effect number and YY is another number for the effect sub connections.
+// The first audio block's input is the output of the preGain block.
+// The last audio block's output is the input of the postGain block.
 // Effects may need multiple sub connections between objects in the effect.
 // Use sensical fomat for other constant connections with postfix _c.
 ///////////////////////////////////////
 
-AudioConnection             lineInToPreGain_c;
-AudioConnection             postGainToLineOut_c;
+AudioConnection             lineInToPreGain_c(lineIn, 0, preGain, 0);
+AudioConnection             postGainToLineOut_c(postGain, 0, lineOut, 0);
 
+// Effect 1: Low Pass Filter
+AudioConnection             effect01Input(preGain, 0, effect01LFP, 0);
+AudioConnection             effect01Output(effect01LPF, 0, postGain, 0);
 
-///////////////////////////////////////
-// Effect switching functions
-///////////////////////////////////////
 
 void T9PB_begin(int memory);
-void T9PB_disconnectAllCords();
+void T9PB_disconnect_all_cords(void);
 void T9PB_hp_volume(float volume) {
     sgtl5000.volume(volume);
 }
 
-// Effect 1: 
-void T9PB_connect_effect_1();
-void T9BP_disconnect_effect_1();
-void T9BP_switch_effect_1();
+///////////////////////////////////////
+// Effect switching functions
+// Functions accept void arguments and return void.
+///////////////////////////////////////
+
+// Effect 1: Low Pass Filter
+void T9PB_connect_effect_01(void);
+void T9BP_disconnect_effect_01(void);
+// void T9BP_switch_effect_1(void);
 
 // Effect 2: 
+
+
+///////////////////////////////////////
+// Effect (dis)connect tables
+///////////////////////////////////////
+void (*T9PB_connectTable[NUM_EFFECTS])(void) = {
+    T9PB_connect_effect_01
+};
+
+void (T9PB_disconnectTable[NUM_EFFECTS])(void) = {
+    T9PB_connect_effect_01
+};
 
 #endif
