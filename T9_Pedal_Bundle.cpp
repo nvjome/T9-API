1cdce945ceb385f1a0ef3eb8479f524a12dc77c8
nvjome <nvjome@gmail.com>
2022-01-23 18:06:58 -0600

    Update T9_Pedal_Bundle.cpp

--- T9_Pedal_Bundle.cpp
+++ T9_Pedal_Bundle.cpp
@@ -110,7 +110,7 @@ void T9PB_begin(void) {
     sgtl5000.enable();
     sgtl5000.inputSelect(AUDIO_INPUT_LINEIN);
     sgtl5000.lineInLevel(0);    // max input level
-    //sgtl5000.lineOutLevel(13);  // max output level
+    sgtl5000.lineOutLevel(13);  // max output level
     sgtl5000.volume(0.3);
 
     preGain.gain(1.0);
